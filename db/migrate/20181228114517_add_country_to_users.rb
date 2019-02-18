13fd96a95c6da320d8c35949ebfd01a43e8a34e2
ashvary <ashvaryvirmani@gmail.com>
2019-02-18 14:26:23 +0530

    table added

--- db/migrate/20181228114517_add_country_to_users.rb
+++ db/migrate/20181228114517_add_country_to_users.rb
@@ -1,6 +1,5 @@
 class AddCountryToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_reference :users, :country, foreign_key: true
     add_reference :users, :state, foreign_key: true
     add_reference :users, :city, foreign_key: true
   end
--- db/migrate/20190215120945_drop_buy_books_table.rb
+++
@@ -1,5 +0,0 @@
-class DropBuyBooksTable < ActiveRecord::Migration[5.2]
-  def change
-		drop_table :buy_books
-  end
-end
