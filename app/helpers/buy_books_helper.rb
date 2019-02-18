d3ecd83c7ecdd3ae1ada8a633654f54415ed77eb
ashvary <ashvaryvirmani@gmail.com>
2019-02-18 17:33:38 +0530

    style added

--- app/helpers/buy_books_helper.rb
+++ app/helpers/buy_books_helper.rb
@@ -1,5 +1,3 @@
 module BuyBooksHelper
-	def parse_created_at(str)
-		
-	end
+
 end
--- db/migrate/20181228114443_remove_country_from_users.rb
+++ db/migrate/20181228114443_remove_country_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveCountryFromUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :country, :string
+    remove_column :users, :country_id, :string
   end
 end
